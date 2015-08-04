9d0bb147db211de77de86bdcc3b5f402361778dc
tunnckoCore <mameto_100@mail.bg>
2015-08-04 08:30:17 +0300

    update deps

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 'use strict';
 
 var octet = require('octet');
-var extend = require('extend');
+var extend = require('extend-shallow');
 
 exports.name = 'octet';
 exports.outputFormat = 'html';
 
 exports.render = function (str, options, locals) {
-  var result = octet(str, extend({}, options, locals));
-  if (result.err) {
-    throw new Error(result.err);
-  }
-  else {
-    return result.res;
-  }
+  return octet(str, extend({}, options, locals));
 };
