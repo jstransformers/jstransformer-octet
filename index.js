33564cae3372ebbdf722a27dafae1084ba908078
tunnckoCore <mameto_100@mail.bg>
2015-09-24 03:04:08 +0300

    bump octet

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var octet = require('octet');
-var extend = require('extend-shallow');
 
 exports.name = 'octet';
 exports.outputFormat = 'html';
 
-exports.render = function (str, options, locals) {
-  return octet(str, extend({}, options, locals));
-};
+exports.compile = octet.compile;
